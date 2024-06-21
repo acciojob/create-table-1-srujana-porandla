961a5e62bbd5a8ae26e6b46e1bfed5a02cf0a26f
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 18:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
+	var t=document.getElementById("sampleTable");
+	var newRow=t.insertRow(0);
+	 var newCell1 = newRow.insertCell(0);
+    var newCell2 = newRow.insertCell(1);
+	newCell1.innerHTML = "New Cell1";
+    newCell2.innerHTML = "New Cell2";
   
   
 }
